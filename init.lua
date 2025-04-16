ada3cd4d016e05732581f5cc0f160af1c7ea764f
Tariq Abbasi <tariq-a@users.noreply.github.com>
2025-04-16 08:38:23 +0100

    Fix issue

--- init.lua
+++ init.lua
@@ -1020,5 +1020,8 @@ require('lazy').setup({
   },
 })
 
+-- Fix for Wezterm issue
+vim.opt.termsync = false
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
